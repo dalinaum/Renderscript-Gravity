9c86b419b3893a32b563a12d5d4bb1a4e8f008bd
Leo Kim <dalinaum@gmail.com>
2011-12-05 01:38:58 +0900

    필요없는 변수 제거

--- src/org/dalinaum/android/gravity/gravity.rs
+++ src/org/dalinaum/android/gravity/gravity.rs
@@ -4,9 +4,6 @@
 
 #include "rs_graphics.rsh"
 
-static int newPart = 0;
-static int initialized = 0;
-
 float gTouchX = 50.f;
 float gTouchY = 50.f;
 
@@ -19,8 +16,7 @@ Point_t *point;
 
 rs_mesh partMesh;
 
-void initParticles()
-{
+void initParticles() {
     int size = rsAllocationGetDimX(rsGetAllocation(point));
     float width = rsgGetWidth();
     float height = rsgGetHeight();
@@ -28,14 +24,13 @@ void initParticles()
     Point_t *p = point;
 
     for (int i = 0; i < size; i++) {
-	p->position.x = rsRand(width);
-	p->position.y = rsRand(height);
-	p->delta.x = 0;
-	p->delta.y = 0;
-	p->color = c;
-	p++;
+        p->position.x = rsRand(width);
+        p->position.y = rsRand(height);
+        p->delta.x = 0;
+        p->delta.y = 0;
+        p->color = c;
+        p++;
     }
-    initialized = 1;
 }
 
 int root() {
@@ -47,28 +42,28 @@ int root() {
     Point_t *p = point;
 
     for (int i = 0; i < size; i++) {
-	float diff_x = gTouchX - p->position.x;
-	float diff_y = gTouchY - p->position.y;
-	float acc = 50.f / (diff_x * diff_x + diff_y * diff_y);
-	float acc_x = acc * diff_x;
-	float acc_y = acc * diff_y;
-	p->delta.x += acc_x;
-	p->delta.y += acc_y;
-	p->position.x += p->delta.x;
-	p->position.y += p->delta.y;
-	p->delta.x *= 0.96;
-	p->delta.y *= 0.96;
-	if (p->position.x > width) {
-	    p->position.x = 0;
-	} else if (p->position.x < 0) {
-	    p->position.x = width;
-	}
-	if (p->position.y > height) {
-	    p->position.y = 0;
-	} else if (p->position.y < 0) {
-	    p->position.y = height;
-	}
-	p++;
+        float diff_x = gTouchX - p->position.x;
+        float diff_y = gTouchY - p->position.y;
+        float acc = 50.f / (diff_x * diff_x + diff_y * diff_y);
+        float acc_x = acc * diff_x;
+        float acc_y = acc * diff_y;
+        p->delta.x += acc_x;
+        p->delta.y += acc_y;
+        p->position.x += p->delta.x;
+        p->position.y += p->delta.y;
+        p->delta.x *= 0.96;
+        p->delta.y *= 0.96;
+        if (p->position.x > width) {
+            p->position.x = 0;
+        } else if (p->position.x < 0) {
+            p->position.x = width;
+        }
+        if (p->position.y > height) {
+            p->position.y = 0;
+        } else if (p->position.y < 0) {
+            p->position.y = height;
+        }
+        p++;
     }
 
     rsgDrawMesh(partMesh);
